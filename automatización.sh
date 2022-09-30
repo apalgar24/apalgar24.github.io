089bfb05987622658d8a0a400bc9924cd7949706
apalgar24 <adrianpalomino68dh@hotmail.com>
2022-09-30 10:33:23 +0200

    Actualización de la página

--- automatización.sh
+++ automatización.sh
@@ -1,14 +1,15 @@
 #Realiza un script bash que te permita automatizar la generación de la página (jekyll) y el despliegue automático de la página en el entorno de producción (git pages).
 
 #Genera la página
+#Proceso en segundo plano
 
-/usr//bin/bundle2.7 exec jekyll serve 
+/usr//bin/bundle2.7 exec jekyll serve &
 
 #Despliega la página
 
 cd _site/
 
-git add . --all || true
+git add *
 
 git commit -am "Actualización de la página"
 
