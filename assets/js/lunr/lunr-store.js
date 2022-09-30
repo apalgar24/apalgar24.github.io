f097b57808d76f90a4856b851605791f3510d9d2
apalgar24 <adrianpalomino68dh@hotmail.com>
2022-09-30 09:53:05 +0200

    Commit inicial

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Página de prueba con Jekyll",
+        "excerpt":"Post 1   descripcion del post 1   Post 2   descripcion del post 2   Post 3   descripcion del post 3  ","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/09/29/welcome-to-jekyll.html",
+        "teaser": null
+      }]
